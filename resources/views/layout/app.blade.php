996f1dd59d2c93460812a21710fd8b2096538474
faidfadjri <faidfadjri.busdev@gmail.com>
2025-06-19 09:24:00 +0700

    fix: appbar

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -35,14 +35,24 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com" rel="preconnect">
     <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
     <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Amatic+SC:wght@400;700&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Rubik:ital,wght@0,300..900;1,300..900&family=Roboto:ital,wght@0,100..900;1,100..900&family=Paytone+One&family=Audiowide&display=swap"
         rel="stylesheet">
 
 
     @livewireStyles
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+    <style>
+        @font-face {
+            font-family: 'Ethnocentric';
+            src: url('/assets/fonts/ethnocentric.otf') format('opentype');
+            font-display: swap;
+        }
+    </style>
 
 </head>
 
--- resources/views/livewire/components/appbar.blade.php
+++ resources/views/livewire/components/appbar.blade.php
@@ -1,89 +1,108 @@
 <div class="sticky top-0 z-50 bg-white shadow-md">
-    <div class="bg-[#F5FBFF] p-4 sticky top-0 z-50 flex items-center">
-        <div class="flex items-center justify-between gap-2 w-full">
-            <div class="flex items-center gap-5">
-                @if (isset($back) && $back)
-                    <a href="{{ $back }}">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
-                        </svg>
-                    </a>
-                @endif
-                @if (isset($title) && $title)
-                    <p class="text-lg font-semibold text-gray-800">
-                        {{ $title }}
-                    </p>
-                @endif
-            </div>
+    <div class="bg-[#F5FBFF] p-4 flex items-center sticky top-0 z-50">
+        <div class="flex items-center justify-between w-full gap-2 lg:gap-10">
 
-            @if (!isset($title) || !$title)
-                <div class="flex items-center flex-1 bg-white border border-gray-500 rounded-md px-3 py-2">
-                    <svg class="w-5 h-5 text-gray-500 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 1010.5 18.5a7.5 7.5 0 006.15-3.85z" />
-                    </svg>
-                    <input type="text" placeholder="Cari di Aling" id="searchbar"
-                        class="bg-transparent outline-none w-full text-sm text-gray-700 placeholder-gray-400"
-                        autocomplete="off" wire:model.live="search" />
+            {{-- Back & Title --}}
+            @if ((isset($back) && $back) || (isset($title) && $title))
+                <div class="flex items-center gap-5">
+                    @if (isset($back) && $back)
+                        <a href="{{ $back }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="size-6" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+                            </svg>
+                        </a>
+                    @endif
+                    @if (isset($title) && $title)
+                        <p class="text-lg font-semibold text-gray-800">{{ $title }}</p>
+                    @endif
                 </div>
             @endif
 
+            {{-- Logo & Search --}}
+            @if (!isset($title) || !$title)
+                <div class="hidden lg:flex items-center gap-3">
+                    <img src="/assets/images/logo.png" alt="logo-brand" class="h-10">
+                    <h1 class="text-xl text-blue-900 font-semibold" style="font-family: 'Audiowide', sans-serif">
+                        Ayam Keliling
+                    </h1>
+                </div>
+
+                {{-- Search Bar + Suggestion Container --}}
+                <div class="relative flex-1">
+                    <div class="flex items-center bg-white border border-gray-500 rounded-md px-3 py-2">
+                        <svg class="w-5 h-5 text-gray-500 mr-2" fill="none" stroke="currentColor"
+                            viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 1010.5 18.5a7.5 7.5 0 006.15-3.85z" />
+                        </svg>
+                        <input type="text" placeholder="Cari di Aling" id="searchbar"
+                            class="bg-transparent outline-none w-full text-sm text-gray-700 placeholder-gray-400"
+                            autocomplete="off" wire:model.live="search" />
+                    </div>
+
+
+
+                    {{-- Search Suggestions --}}
+                    <div class="absolute left-0 right-0 w-full z-40 bg-white shadow-xl rounded-b-md">
+
+                        {{-- Loading Indicator --}}
+                        <div class="w-full flex justify-center items-center p-4" wire:loading>
+                            <dotlottie-player src="/assets/lotties/loading.lottie" background="transparent"
+                                speed="1" style="width: 35px; height: 35px;" class="mx-auto" loop
+                                autoplay></dotlottie-player>
+                        </div>
+
+                        @if ($isFocused)
+                            <div class="p-4">
+                                <div class="mb-6">
+                                    <h2 class="font-semibold text-gray-800 mb-3">Pencarian Terakhir</h2>
+                                    <div id="history-container" class="flex flex-wrap gap-2">
+                                        @foreach ($searchHistory as $history)
+                                            <button wire:click="$set('search', '{{ $history }}')"
+                                                class="px-4 py-2 border border-gray-400 bg-black/1 text-gray-700 rounded-full text-sm">
+                                                {{ $history }}
+                                            </button>
+                                        @endforeach
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <h2 class="font-semibold text-gray-800 flex items-center gap-2 mb-3">
+                                        <span>👍</span> Best Seller
+                                    </h2>
+                                    <div class="flex items-center gap-3 p-3 bg-white rounded-xl shadow-sm w-full mb-4">
+                                        <img src="https://www.sinarpahalautama.com/image-product/img61-1581762923.jpg"
+                                            alt="Ayam Kampung" class="w-10 h-10 rounded-full object-cover" />
+                                        <span class="text-sm font-medium text-gray-800">Ayam Kampung 10KG</span>
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            @endif
+
+            {{-- Cart or Cancel Button --}}
             @if (!$isFocused)
                 @if (!isset($back) && !$back)
                     <div class="cart relative ml-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="size-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="size-6" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                         </svg>
                         <span
-                            class="absolute -top-1 -right-1 flex items-center justify-center h-3 w-3 rounded-full bg-orange-400 text-white text-xs font-bold ring-2 ring-white">
+                            class="absolute -top-1 -right-1 h-3 w-3 flex items-center justify-center rounded-full bg-orange-400 text-white text-xs font-bold ring-2 ring-white">
                             2
                         </span>
                     </div>
                 @endif
-            @endif
-
-            @if ($isFocused)
+            @else
                 <button class="font-semibold ml-2 cursor-pointer" wire:click="disableFocus">Batal</button>
             @endif
 
         </div>
-    </div>
-
-    <div>
-        <div class="w-full flex flex-col items-center justify-center" wire:loading>
-            <dotlottie-player src="/assets/lotties/loading.lottie" background="transparent" speed="1"
-                style="width: 35px; height: 35px; display: flex; margin: auto;" loop autoplay></dotlottie-player>
-        </div>
-        @if ($isFocused)
-            <div class="p-4 bg-white shadow-xl rounded-b-md mb-4">
-                <div class="mb-6">
-                    <h2 class="font-semibold text-gray-800 mb-3">Pencarian Terakhir</h2>
-                    <div id="history-container" class="flex flex-wrap gap-2">
-                        @foreach ($searchHistory as $history)
-                            <button wire:click="$set('search', '{{ $history }}')"
-                                class="px-4 py-2 border border-gray-400 bg-black/1 text-gray-700 rounded-full text-sm">
-                                {{ $history }}
-                            </button>
-                        @endforeach
-                    </div>
-                </div>
-
-                <div>
-                    <h2 class="font-semibold text-gray-800 flex items-center gap-2 mb-3">
-                        <span>👍</span> Best Seller
-                    </h2>
-
-                    <div class="flex items-center gap-3 p-3 bg-white rounded-xl shadow-sm w-full mb-4">
-                        <img src="https://www.sinarpahalautama.com/image-product/img61-1581762923.jpg"
-                            alt="Ayam Kampung" class="w-10 h-10 rounded-full object-cover" />
-                        <span class="text-sm font-medium text-gray-800">Ayam Kampung 10KG</span>
-                    </div>
-                </div>
-            </div>
-        @endif
     </div>
 </div>
